1256b8155de658544a0f60814ebb0b8488475033
Daniel Suo <danielsuo@gmail.com>
2016-04-26 21:52:12 -0400

    Make build into package

--- build/js/potree.js
+++ build/js/potree.js
@@ -1,5 +1,3 @@
-export default function(THREE) {
-
 function Potree(){
 	
 }
@@ -9654,5 +9652,15 @@ Object.defineProperty(Potree.PointCloudArena4DGeometry.prototype, "spacing", {
 	}
 });
 
-return Potree;
-};
+// Export the Potree object for **Node.js**, with
+// backwards-compatibility for the old `require()` API. If we're in
+// the browser, add `_` as a global object via a string identifier,
+// for Closure Compiler "advanced" mode.
+if (typeof exports !== 'undefined') {
+  if (typeof module !== 'undefined' && module.exports) {
+    exports = module.exports = Potree;
+  }
+  exports.Potree = Potree;
+} else {
+  this['Potree'] = Potree;
+}
